124335e2dd7819bf325df2afd65ab858bddbfb8b
Rashini Rajasinghe <rashirajasinghe@gmail.com>
2025-08-28 11:10:05 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,13 +11,13 @@ const Home = () => {
                     className='text-5xl sm:text-6xl lg:text-7xl font-bold'>We serve the richest <span data-aos='zoom-out' data-aos-delay="300"  className='text-primary font-cursive'>coffee</span> in the city
                     </h1>
                     <div data-aos='fade-up' data-aos-delay="400">
-                        <button className='bg-gradient-to-r from-primary to-secondary border-2 border-primary rounded-full px-4 py-2 text-white hover:scale-105 duration-200'>Coffee And Code</button>
+                        <button className='bg-gradient-to-r from-primary to-secondary border-2 border-primary rounded-full px-4 py-2 text-white hover:scale-105 duration-200'>Coffee And enjoy</button>
                     </div>
                 </div>
                 <div data-aos="zoom-in" className='min-h-[450px] flex justify-center items-center order-1 sm:order-2 relative'>
                     <img src={HeroImg} alt='' className='w-[300px] sm:w-[450px] sm:scale-110 mx-auto spin'></img>
                     <div data-aos='fade-left' className='bg-gradient-to-r from-primary to-secondary absolute top-10 left-10 p-3 rounded-xl'>
-                        <h1>Hey Coder</h1>
+                        <h1>Hey Coffee lovers</h1>
                     </div>
                     <div data-aos='fade-right' className='bg-gradient-to-r from-primary to-secondary absolute bottom-10 right-10 p-3 rounded-xl'>
                         <h1>Best Coffee</h1>
@@ -31,5 +31,6 @@ const Home = () => {
     </div>
     );
 };
+
 
 export default Home;
